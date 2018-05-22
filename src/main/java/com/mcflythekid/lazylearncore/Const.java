43b30f2c6e59a26207cb76ec106dd01e04176bfe
Marty Nguyen <odopoc@gmail.com>
2018-05-22 07:20:59 +0700

    Change dev

--- src/main/java/com/mcflythekid/lazylearncore/Const.java
+++ src/main/java/com/mcflythekid/lazylearncore/Const.java
@@ -8,7 +8,7 @@ import java.util.Map;
  * @JsonFormat(pattern = "", timezone = "")
  */
 public final class Const {
-    public static final Integer PARAM_PORT = 8088;
+    public static final Integer PARAM_PORT = 8084;
     public static final String PARAM_JSON_DATETIMEFORMAT = "yyyy-MM-dd HH:mm:ss";
 
     public static final String PARAM_JSON_TIMEZONE = "GMT+7";
