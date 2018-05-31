42bdd739d62f8ca87d388f7c6670705f8287fecb
Marty Nguyen <odopoc@gmail.com>
2018-05-31 13:50:27 +0700

    revert test

--- script/jenkins_prod.sh
+++ script/jenkins_prod.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo 'clmcc'
-#/bin/cp -rf /opt/lazylearn-api/application.properties src/main/resources/application.properties
-#mvn clean install -DskipTests
-#mv -f target/*.war /var/lib/tomcat8/webapps/lazylearn-api.war
+/bin/cp -rf /opt/lazylearn-api/application.properties src/main/resources/application.properties
+mvn clean install -DskipTests
+mv -f target/*.war /var/lib/tomcat8/webapps/lazylearn-api.war
