b1c5456dcaa60ddb35b2d655a688cf3f93db2b4b
Marty Nguyen <odopoc@gmail.com>
2018-05-31 13:44:58 +0700

    test

--- script/jenkins_prod.sh
+++ script/jenkins_prod.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-/bin/cp -rf /opt/lazylearn-api/application.properties src/main/resources/application.properties
-mvn clean install -DskipTests
-mv -f target/*.war /var/lib/tomcat8/webapps/lazylearn-api.war
+echo 'clmcc'
+#/bin/cp -rf /opt/lazylearn-api/application.properties src/main/resources/application.properties
+#mvn clean install -DskipTests
+#mv -f target/*.war /var/lib/tomcat8/webapps/lazylearn-api.war
