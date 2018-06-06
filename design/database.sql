15c1ea5ec918ececefffae8673910613d78f1468
McFly the Kid <odopoc@gmail.com>
2018-06-07 00:09:09 +0700

    Update database

--- design/database.sql
+++ design/database.sql
@@ -1,54 +1,64 @@
--- [mysqld]
--- character-set-server = utf8mb4
--- collation-server = utf8mb4_unicode_ci
-
-DROP TABLE IF EXISTS `user`;
-CREATE TABLE `user` (
-  `id` VARCHAR(100) PRIMARY KEY,
-  `email` VARCHAR(100) UNIQUE,
-  `hashedpassword` VARCHAR(100),
-  `createdon` DATETIME,
-  `updatedon` DATETIME,
-  `registeripaddress` VARCHAR(100)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
-
-DROP TABLE IF EXISTS `card`;
 CREATE TABLE `card` (
-  `id` VARCHAR(100) PRIMARY KEY,
-  `userid` VARCHAR(100),
-  `deckid` VARCHAR(100),
-  `front` LONGTEXT,
-  `back` LONGTEXT,
-  `step` INT,
-  `wakeupon` DATETIME,
-  `createdon` DATETIME,
-  `updatedon` DATETIME,
-  `learnedon` DATETIME,
-  INDEX(`userid`),
-  INDEX(`deckid`)
+  `id` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `userid` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `deckid` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `front` longtext COLLATE utf8mb4_bin,
+  `back` longtext COLLATE utf8mb4_bin,
+  `step` int(11) DEFAULT NULL,
+  `wakeupon` datetime DEFAULT NULL,
+  `createdon` datetime DEFAULT NULL,
+  `updatedon` datetime DEFAULT NULL,
+  `learnedon` datetime DEFAULT NULL,
+  `archived` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `userid` (`userid`) USING BTREE,
+  KEY `deckid` (`deckid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-DROP TABLE IF EXISTS `deck`;
 CREATE TABLE `deck` (
-  `id` VARCHAR(100) PRIMARY KEY,
-  `userid` VARCHAR(100),
-  `name` VARCHAR(100),
-  `createdon` DATETIME,
-  `updatedon` DATETIME,
-  INDEX(`userid`)
+  `id` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `userid` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `createdon` datetime DEFAULT NULL,
+  `updatedon` datetime DEFAULT NULL,
+  `hyperdeckid` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `hyperdecktype` int(11) DEFAULT NULL,
+  `archived` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `userid` (`userid`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
-DROP TABLE IF EXISTS `forgetpassword`;
-CREATE TABLE `forgetpassword` (
-  `id` VARCHAR(100) PRIMARY KEY,
-  `userid` VARCHAR(100),
-  `status` INT,
-  `currentemail` VARCHAR(100),
-  `ipaddress` VARCHAR(100),
-  `createdon` DATETIME,
-  `updatedon` DATETIME,
-  `expiredon` DATETIME,
-  INDEX(`userid`),
-  INDEX(`currentemail`)
+CREATE TABLE `forget_password` (
+  `id` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `userid` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `status` int(11) DEFAULT NULL,
+  `currentemail` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `ipaddress` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `createdon` datetime DEFAULT NULL,
+  `updatedon` datetime DEFAULT NULL,
+  `expiredon` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  KEY `userid` (`userid`) USING BTREE,
+  KEY `currentemail` (`currentemail`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
+CREATE TABLE `user` (
+  `id` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `hashedpassword` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `createdon` datetime DEFAULT NULL,
+  `updatedon` datetime DEFAULT NULL,
+  `registeripaddress` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `jtv` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `facebookid` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `fullname` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE,
+  UNIQUE KEY `email` (`email`) USING BTREE,
+  UNIQUE KEY `user_un` (`facebookid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+
+CREATE TABLE `user_authority` (
+  `userid` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  `authority` varchar(100) COLLATE utf8mb4_bin NOT NULL,
+  PRIMARY KEY (`userid`,`authority`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
--- design/view.sql
+++ design/view.sql
@@ -1,16 +1,65 @@
-SELECT 
-	d.id
-	,d.userid
-	,d.`name`
-	,d.createdon
-	,d.updatedon
-	,COUNT(c.id) AS totalcard
-	,SUM(IF(c.wakeupon < NOW(), 1, 0)) AS totaltimeupcard
-FROM deck d
-LEFT JOIN card c ON d.id = c.deckid
-GROUP BY
-	d.id
-	,d.userid
-	,d.`name`
-	,d.createdon
-	,d.updatedon 
+-- vdeck
+select
+    `d`.`id` as `id`,
+    `d`.`userid` as `userid`,
+    `d`.`name` as `name`,
+    `d`.`createdon` as `createdon`,
+    `d`.`updatedon` as `updatedon`,
+    `d`.`archived` as `archived`,
+    count( `c`.`id` ) as `totalcard`,
+    sum( if(( `c`.`wakeupon` < now()), 1, 0 )) as `totaltimeupcard`
+from
+    (
+        `lazylearn_api`.`deck` `d`
+    left join `lazylearn_api`.`card` `c` on
+        (
+            (
+                `d`.`id` = `c`.`deckid`
+            )
+        )
+    )
+group by
+    `d`.`id`,
+    `d`.`userid`,
+    `d`.`name`,
+    `d`.`createdon`,
+    `d`.`updatedon`,
+    `d`.`archived`;
+
+-- vuser
+select
+    `u`.`id` as `id`,
+    `u`.`email` as `email`,
+    `u`.`registeripaddress` as `registeripaddress`,
+    `u`.`createdon` as `createdon`,
+    `u`.`updatedon` as `updatedon`,
+    `u`.`fullname` as `fullname`,
+    `u`.`facebookid` as `facebookid`,
+    count( distinct `c`.`id` ) as `cards`,
+    count( distinct `d`.`id` ) as `decks`
+from
+    (
+        (
+            `lazylearn_api`.`user` `u`
+        left join `lazylearn_api`.`deck` `d` on
+            (
+                (
+                    `u`.`id` = `d`.`userid`
+                )
+            )
+        )
+    left join `lazylearn_api`.`card` `c` on
+        (
+            (
+                `u`.`id` = `c`.`userid`
+            )
+        )
+    )
+group by
+    `u`.`id`,
+    `u`.`email`,
+    `u`.`registeripaddress`,
+    `u`.`createdon`,
+    `u`.`updatedon`,
+    `u`.`fullname`,
+    `u`.`facebookid`;
