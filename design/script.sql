50c3e11c64d84a850fcfd296cc42b176b5fc5e0c
McFly the Kid <odopoc@gmail.com>
2018-06-15 15:05:02 +0700

    Remove TODO

--- design/script.sql
+++ design/script.sql
@@ -1,17 +1,3 @@
-CREATE TABLE lazylearn_api.todo (
-	id varchar(100) NULL,
-	userid varchar(100) NULL,
-	done varchar(100) NULL,
-	createddate datetime NULL,
-	updateddate datetime NULL,
-	`data` longtext NULL,
-	CONSTRAINT todo_pk PRIMARY KEY (id)
-)
-ENGINE=InnoDB
-DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_bin;
-CREATE INDEX userid USING BTREE ON lazylearn_api.todo (userid) ;
-
 
 CREATE TABLE lazylearn_api.vocab (
 	id varchar(100) NULL,
@@ -31,7 +17,7 @@ COLLATE=utf8mb4_bin;
 CREATE INDEX vocab_userid_idx USING BTREE ON lazylearn_api.vocab (userid) ;
 
 
-ALTER TABLE lazylearn_api.todo MODIFY COLUMN done int NULL;
+
 ALTER TABLE lazylearn_api.card MODIFY COLUMN archived int(11) NULL;
 ALTER TABLE lazylearn_api.deck MODIFY COLUMN archived int(11) NULL;
 ALTER TABLE lazylearn_api.deck DROP COLUMN hyperdeckid;
