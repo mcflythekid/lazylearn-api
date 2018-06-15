549d9022a1ab3eec81cabf499d65c137c4090e28
McFly the Kid <odopoc@gmail.com>
2018-06-15 15:01:13 +0700

    add vocab and todo controller

---
+++ src/main/java/com/mcflythekid/lazylearncore/controller/VocabController.java
@@ -0,0 +1,12 @@
+package com.mcflythekid.lazylearncore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author McFly the Kid
+ */
+@RestController
+@RequestMapping("/vocab")
+public class VocabController extends BaseController{
+}
