ee88a8104d749ae824aff67dec0720e36ebd6e24
Tuan Nguyen <odopoc@gmail.com>
2018-11-14 01:34:28 +0700

    change welceome deck

--- src/main/java/com/lazylearn/api/service/AuthService.java
+++ src/main/java/com/lazylearn/api/service/AuthService.java
@@ -179,7 +179,7 @@ public class AuthService {
     }
 
     private Deck createTemplateDeck(String userId) throws IOException {
-        final String TEMPLATE_ID = "./deck/3000.txt";
+        final String TEMPLATE_ID = "./deck/100.txt";
         return deckService.importDeck(TEMPLATE_ID, userId);
     }
 }
