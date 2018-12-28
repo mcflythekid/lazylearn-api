a2117d8325b747bf2b3287774f01af1d81728872
Marty Nguyen <odopoc@gmail.com>
2018-12-29 03:17:26 +0700

    Add program controller

---
+++ src/main/java/com/lazylearn/api/controller/ProgramController.java
@@ -0,0 +1,25 @@
+package com.lazylearn.api.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/program")
+public class ProgramController {
+
+	private static Map<String, String> ALL_PROGRAM = new HashMap<>();
+	static {
+		ALL_PROGRAM.put("pimsleur", "Pimsleur");
+		ALL_PROGRAM.put("sm2", "SuperMemo2");
+	}
+	
+	@GetMapping("/all")
+	public Object getAll() {
+		return ALL_PROGRAM;
+	}
+	
+}
