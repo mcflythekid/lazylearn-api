516a060aacc58a11d87e94e8412c1213ecbf3b0b
Marty Nguyen <odopoc@gmail.com>
2018-12-29 03:38:27 +0700

    Refactor class name

--- src/main/java/com/lazylearn/api/program/PimsleurProgram.java
+++ src/main/java/com/lazylearn/api/program/PimsleurProgram.java
@@ -4,6 +4,6 @@ import com.lazylearn.api.config.Consts;
 import org.springframework.stereotype.Service;
 
 @Service(Consts.STEP_PROGRAM__PIMSLEUR)
-public class PimsleurStepProgram extends StaticStepProgram {
+public class PimsleurProgram extends StaticStepProgram {
 
 }
