00bac6a137c6852a6e6f3070960ba582b89a710b
Marty McFly <odopoc@gmail.com>
2020-05-13 20:23:40 +0700

    Update count for onetime today learn

--- src/main/java/com/lazylearn/api/controller/LearnController.java
+++ src/main/java/com/lazylearn/api/controller/LearnController.java
@@ -18,8 +18,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Map;
+import java.util.TimeZone;
 
 /**
  * @author McFly the Kid
@@ -51,6 +54,44 @@ public class LearnController extends BaseController{
     @GetMapping("/count-onetime-learn-card")
     public Map countOnetimeLearnCard() throws Exception {
         return ImmutableMap.of("count", cardRepo.countAllByUserIdAndWakeupOnBefore(getUserId(), new Date()));
+    }
+
+    public static Date getRemoteTimezoneMidnightDate(String remoteTimezone) throws ParseException {
+        final String fullFormat = "yyyy-MM-dd HH:mm:ss";
+        final String shortFormat = "yyyy-MM-dd";
+        SimpleDateFormat localFullFormatter = new SimpleDateFormat(fullFormat);
+        SimpleDateFormat remoteShortFormatter = new SimpleDateFormat(shortFormat);
+        SimpleDateFormat remoteFullFormatter = new SimpleDateFormat(fullFormat);
+        remoteShortFormatter.setTimeZone(TimeZone.getTimeZone(remoteTimezone));
+        remoteFullFormatter.setTimeZone(TimeZone.getTimeZone(remoteTimezone));
+
+        String remoteShortNow = remoteShortFormatter.format(new Date());
+        String remoteFullAtRemoteMidnightString = remoteShortNow + " 00:00:00";
+        Date remoteFullAtRemoteMidnightDate = remoteFullFormatter.parse(remoteFullAtRemoteMidnightString);
+
+        return remoteFullAtRemoteMidnightDate;
+    }
+
+    public static void main(String[] args) throws ParseException {
+        String format = "yyyy-MM-dd HH:mm:ss";
+        for (int i = 12; i >= 1; i--){
+            String gmt = "GMT+" + i;
+            Date date = getRemoteTimezoneMidnightDate(gmt);
+            String s = new SimpleDateFormat(format).format(date);
+            System.out.println(gmt + ": " + s);
+        }
+        for (int i = 1; i <= 12; i++){
+            String gmt = "GMT-" + i;
+            Date date = getRemoteTimezoneMidnightDate(gmt);
+            String s = new SimpleDateFormat(format).format(date);
+            System.out.println(gmt + ": " + s);
+        }
+    }
+
+    @GetMapping("/count-today-onetime-learn-card")
+    public Map countTodayOnetimeLearnCard() throws Exception {
+        User user = userRepo.findOne(getUserId());
+        return ImmutableMap.of("count", cardRepo.countAllByUserIdAndWakeupOnBeforeAndLearnedOnBefore(getUserId(), new Date(), getRemoteTimezoneMidnightDate(user.getTimezone())));
     }
 
     @GetMapping("/get-deck")
