d55d8c21e8a91e211305e12c858d9b156ce96354
mc <odopoc@gmail.com>
2021-06-09 20:03:41 +0700

    - add file

---
+++ src/main/java/com/lazylearn/api/config/env/WiredEnv.java
@@ -0,0 +1,31 @@
+package com.lazylearn.api.config.env;
+
+import lombok.Data;
+
+@Data
+public class WiredEnv {
+    //db
+    private String dbUrl;
+    private String dbUsername;
+    private String dbPassword;
+    private boolean dbShowSql;
+    private String dbDriver;
+    private String dbDialect;
+    private Integer dbPoolSize;
+
+    //jwf
+    private String jwtSecret;
+
+    //file
+    private String fileUpload;
+    private String fileUrl;
+
+    private String resetPassword;
+
+    private String mailServer;
+    private Integer mailPort;
+
+    private boolean telegramEnabled;
+
+
+}
